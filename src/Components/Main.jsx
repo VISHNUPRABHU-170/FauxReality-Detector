2d305ef9a476a7ce2c9b1cb6f1fd51ca12d4440e
VISHNUPRABHU <vishnurr1708@gmail.com>
2023-07-23 23:04:24 +0530

    Added login page

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,49 +1,59 @@
 import icon from "./icon.jpg";
 import { useState } from "react";
-import Uploadicon from "./upload icon.png"
-import fake from "./fake.png";
+import Uploadicon from "./upload icon.png";
+import Google from "./google-icon.png";
 
 
 function Main() {
 
     const [show, setShow] = useState(false);
+    const [inn, setIn] = useState(false);
+    const [up, setUp] = useState(false);
 
     return (
         <div>
             <div className="text-white bg-blue-500 shadow-xl w-full h-[55px] sm:h-[70px] flex items-center sm:grid sm:grid-cols-3">
-                <img src={icon} alt="icon" className="hidden sm:block w-[60px] h-full sm:h-[70px] sm:w-[90px]" />
-                <img src={fake} alt="icon" className="md:hidden w-[70px] h-[75px] ml-[-10px] mt-1" />
-                <h1 className="drop-shadow-xl text-[23px] px-[-30px] ml-[-10px] sm:text-4xl font-bold sm:px-4 sm:ml-[-420px]">
+                <img src={icon} alt="icon" className="w-[60px] h-full sm:h-[70px] sm:w-[90px]" />
+                <h1 className="drop-shadow-xl text-[23px] px-4 ml-[-10px] sm:text-4xl font-bold sm:px-4 sm:ml-[-420px]">
                     FauxReality Detector
                 </h1>
                 <div className="hidden md:block sm:ml-[-100px]">
                     <nav className="px-[100px] ml-[200px] text-xl font-semibold">
                         <ul className="grid grid-cols-3 gap-[80px]">
                             <li className="py-2 hover:text-[25px]">
-                                <a href="/">Detect</a>
+                                <a href="/">
+                                    <button onClick={() => { setShow(!show) }}>
+                                        Detect
+                                    </button>
+                                </a>
                             </li>
                             <li className="py-2 hover:text-[25px]">
-                                <a href="/signin">Login</a>
+                                <a>
+                                    <button onClick={() => { setIn(!inn) }}>
+                                        Login
+                                    </button>
+                                </a>
                             </li>
                             <li className="bg-white rounded-md drop-shadow-lg text-blue-500 w-[95px] h-[45px] flex justify-center hover:bg-blue-900 hover:text-white">
-                                <a href="/signup" className="py-2">SignUp</a>
+                                <a className="py-2">
+                                    <button onClick={() => { setUp(!up) }}>
+                                        SignUp
+                                    </button>
+                                </a>
                             </li>
                         </ul>
                     </nav>
                 </div>
-                <div className="sm:hidden flex justify-end ml-10">
+                <div className="sm:hidden flex justify-end">
                     <button
                         onClick={() => {
                             setShow(!show);
                         }}
-                        className="text-xl font-semibold bg-white text-blue-400 rounded-lg drop-shadow-xl w-[75px] h-[40px] hover:bg-gray-600 hover:text-white"
+                        className="text-xl ml-3 font-semibold bg-white text-blue-400 rounded-lg drop-shadow-xl w-[75px] h-[40px] hover:bg-gray-600 hover:text-white"
                     >
                         SignUp
                     </button>
                 </div>
-            </div>
-            <div className="sm:hidden">
-                {show && <Navbar />}
             </div>
             <div className="flex justify-center py-[100px] sm:py-[90px]">
                 <div className="h-[450px] w-[350px] sm:h-[500px] sm:w-[700px] bg-blue-300 shadow-xl rounded-xl">
@@ -72,23 +82,38 @@ function Main() {
                     </div>
                 </div>
             </div>
+            {show && <Navbar />}
+            {inn && <Login />}
+            {up && <Signup />}
         </div>
     );
 
-    function Navbar(){
-        return(
-            <div className="sm:hidden py-[55px] text-black fixed inset-0 flex items-start justify-end bg-opacity-75 bg-gray-500 z-50">
+    function Navbar() {
+        return (
+            <div className="sm:hidden py-[55px] text-black fixed inset-0 flex items-start justify-end z-20">
                 <div className="bg-white rounded-lg h-[165px] w-[130px] text-xl mr-2">
                     <nav>
                         <ul className="mt-0 grid grid-cols-1 gap-2 font-semibold">
                             <li className="flex justify-center h-[50px] hover:bg-black hover:rounded-lg hover:text-white">
-                                <a href="/" className="mt-2">Detect</a>
+                                <a href="/" className="mt-2"><button onClick={() => { setShow(!show) }}>
+                                    Detect
+                                </button></a>
                             </li>
                             <li className="flex justify-center h-[50px] hover:bg-black hover:rounded-lg hover:text-white">
-                                <a href="/signin" className="mt-2">Login</a>
+                                <a className="mt-2"><button onClick={() => {
+                                    setIn(!inn)
+                                    setShow(!show)
+                                }}>
+                                    Login
+                                </button></a>
                             </li>
                             <li className="flex justify-center h-[50px] hover:bg-black hover:rounded-lg hover:text-white">
-                                <a href="/signup" className="mt-2">Signup</a>
+                                <a className="mt-2"><button onClick={() => {
+                                    setUp(!up)
+                                    setShow(!show)
+                                }}>
+                                    SignUp
+                                </button></a>
                             </li>
                         </ul>
                     </nav>
@@ -96,6 +121,71 @@ function Main() {
             </div>
         );
     };
+
+    function Login() {
+        return (
+            <div>
+                <div className="fixed inset-0 flex items-center justify-center bg-opacity-75 bg-gray-500 z-50">
+                    <div className="bg-white sm:w-[700px] sm:h-[560px]">
+                        <form className="grid grid-cols-1 gap-[20px] py-[50px] text-lg">
+                            <div className="flex justify-center text-4xl font-bold">
+                                Login
+                            </div>
+                            <div className="flex justify-center mt-10 font-semibold">
+                                <div className="grid grid-cols-1">
+                                    <h1 className="text-gray-500">USERNAME</h1>
+                                    <input type="text" className="bg-gray-100 border-2 w-[300px] border-gray-300 rounded-md p-2" />
+                                </div>
+                            </div>
+                            <div className="flex justify-center  font-semibold">
+                                <div className="grid grid-cols-1">
+                                    <h1 className="text-gray-500">PASSWORD</h1>
+                                    <input type="text" className="bg-gray-200 border-2 w-[300px] border-gray-300 rounded-md p-2" />
+                                </div>
+                            </div>
+                            <div className="flex justify-center  font-semibold">
+                                <button 
+                                type="submit" 
+                                className="ml-2 mt-[-8px] w-[100px] h-[40px] text-blue-400 rounded-lg shadow-xl hover:bg-blue-600 hover:text-white"
+                                onClick={() => {setIn(!inn)}}>
+                                    Login
+                                </button>
+                            </div>
+                            <div className="flex justify-center">
+                                <div className="flex items-center bg-white border-2 border-gray-800 rounded-lg h-[40px] w-[300px] font-semibold">
+                                    <img src={Google} alt="Google" className="px-10 w-15 h-7"/>
+                                    <p className="ml-[-30px]">Continue with Google</p>
+                                </div>
+                            </div>
+                            <div className="flex justify-center">
+                                <p className="ml-6">Don't have an Account yet?</p>
+                                <button 
+                                className="ml-2 mt-[-8px] w-[100px] h-[40px] text-blue-400 rounded-lg shadow-xl hover:bg-blue-600 hover:text-white"
+                                onClick={() => {setUp(!up)
+                                setIn(!inn)}}>
+                                    Signup
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    function Signup() {
+        return (
+            <div>
+                <div className="hidden md:block">
+                    <div className="fixed inset-0 flex items-center justify-center bg-opacity-75 bg-gray-500 z-50">
+                        <div className="bg-white sm:w-[700px] sm:h-[560px]">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default Main;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,21 @@
 function Navbar(){
     return(
-        <div className="sm:hidden text-black fixed inset-0 flex items-center justify-center bg-opacity-75 bg-gray-500 z-50">
-            Navbar
+        <div className="sm:hidden py-[55px] text-black fixed inset-0 flex items-start justify-end z-50">
+            <div className="bg-white rounded-lg h-[165px] w-[130px] text-xl mr-2">
+                <nav>
+                    <ul className="mt-0 grid grid-cols-1 gap-2 font-semibold">
+                        <li className="flex justify-center h-[50px] hover:bg-black hover:rounded-lg hover:text-white">
+                            <a href="/" className="mt-2">Detect</a>
+                        </li>
+                        <li className="flex justify-center h-[50px] hover:bg-black hover:rounded-lg hover:text-white">
+                            <a href="/signin" className="mt-2">Login</a>
+                        </li>
+                        <li className="flex justify-center h-[50px] hover:bg-black hover:rounded-lg hover:text-white">
+                            <a href="/signup" className="mt-2">Signup</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     );
 };
