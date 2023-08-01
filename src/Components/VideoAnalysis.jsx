049b16591ce8fcac376930aa6cf4d8480fe17a1b
VISHNUPRABHU <vishnurr1708@gmail.com>
2023-08-01 18:36:48 +0530

    updated

--- src/Components/VideoAnalysis.jsx
+++ src/Components/VideoAnalysis.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+//video analysis component
+//Frames detection
+
 function VideoAnalysis() {
 
     const [file, setFile] = useState(null);
